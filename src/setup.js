75d202808729d39c9be63a3b30685c76ae75b6bb
dado-offbrand <dogeychicken47@gmail.com>
2023-10-02 16:02:40 -0400

    Fixed automation error for open SSL byass
    Accidentally called for start command after setup instead of build command

--- src/setup.js
+++ src/setup.js
@@ -1,3 +1,3 @@
 process.env.NODE_OPTIONS = '--openssl-legacy-provider';
 const child_process = require('child_process');
-child_process.execSync('npm run start', { stdio: 'inherit' });
+child_process.execSync('npm run build', { stdio: 'inherit' });
