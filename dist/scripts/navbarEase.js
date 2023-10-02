5f84fae746c02da3b185645af176fa571038415b
dado-offbrand <dogeychicken47@gmail.com>
2023-10-02 19:22:15 -0400

    Adjusted navbar order, added new page

--- dist/scripts/navbarEase.js
+++ dist/scripts/navbarEase.js
@@ -1,7 +1,8 @@
-let names = [ /*n1*/ "Home", /*n2*/ "Project List", /*n3*/ "Current Project", /*n4*/ "Playground"];
+//let names = [ /*n1*/ "Home", /*n2*/ "Project List", /*n3*/ "Current Project", /*n4*/ "Playground"];
+let names = [ /*n1*/ "Home", /*n2*/ "What I Do", /*n3*/ "Projects", /*n4*/ "Current Project", /*n5*/ "Playground"];
 
 function configNav() {
-    for (let i = 1; i <= 4; i++) 
+    for (let i = 1; i <= names.length; i++) 
     {
         document.getElementById('n'+i).textContent = names[i-1];
     }
